c5b12d0fba7e621d78d8df5bd17eabd1510d30b9
dionatanribeiro <dionatangil@gmail.com>
2015-07-07 07:10:48 -0300

    fix(curso-dao): Adicionado anotação para funcionar injecao de dependencia

--- EuroSchool/src/br/edu/qi/euroschool/dao/CursoDao.java
+++ EuroSchool/src/br/edu/qi/euroschool/dao/CursoDao.java
@@ -2,10 +2,15 @@ package br.edu.qi.euroschool.dao;
 
 import java.util.List;
 
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
 import br.edu.qi.euroschool.core.AbstractDao;
 import br.edu.qi.euroschool.core.TemplateDao;
 import br.edu.qi.euroschool.model.weak.Curso;
 
+@Stateless
+@Local
 public class CursoDao extends TemplateDao<Curso> implements AbstractDao<Curso> {
 
 	@Override
