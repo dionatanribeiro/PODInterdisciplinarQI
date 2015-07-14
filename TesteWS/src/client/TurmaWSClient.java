894074c89c284dbeac6b3fe86b5981f3e0d22aee
jonatastrife <jonatastrife03@gmail.com>
2015-07-13 21:41:50 -0300

    Sei lá que merda era pra ser isso.

---
+++ TesteWS/src/client/TurmaWSClient.java
@@ -0,0 +1,8 @@
+package client;
+
+import javax.xml.ws.WebServiceRef;
+
+@WebServiceRef(wsdlLocation = "http://localhost:8080/TesteWebService/TurmaWSService?wsdl")
+public class TurmaWSClient {
+
+}
